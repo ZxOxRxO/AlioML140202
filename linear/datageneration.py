5b73c1a3f9b0c7ee0033523ba79935fd2aa4b39e
aligharaghani0 <aligharaghani0@gmail.com>
2024-05-18 01:46:04 +0330

    adding the figure data opton

--- linear/datageneration.py
+++ linear/datageneration.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
@@ -8,14 +9,14 @@ def f(x1 , x2  ):
 
 
 
-X1 = np.random.uniform(0, 10, 100)
-X2 = np.random.uniform(0, 10, 100)
+X1 = np.random.uniform(0, 10, 50)
+X2 = np.random.uniform(0, 10, 50)
 y_true = f(X1, X2)
-noise = np.random.normal(0, 10, 100)
+noise = np.random.normal(0,2, 50)
 y_noisy = y_true + noise
 
 
-noise = np.random.standard_t(10, 100)
+noise = np.random.standard_t(10, 50)
 y_noisy = y_true + noise
 
 
@@ -23,4 +24,24 @@ y_noisy = y_true + noise
 data = pd.DataFrame({'X': X1, 'y_noisy': y_noisy})
 
 
-data.to_excel('./linear/LinearData.xlsx', index=False)
+data.to_excel('./LinearData.xlsx', index=False)
+
+
+
+
+
+df = pd.read_excel('LinearData.xlsx')
+X = df[['X']]
+y = df['y_noisy']
+
+# plt.plot(df['X'], df['y_noisy'])
+fig = plt.figure()
+ax = fig.add_subplot()
+
+ax.scatter(X, y, cmap='viridis')
+
+plt.xlabel('X')
+plt.ylabel('y_noisy')
+plt.title('X1 vs y_noisy')
+plt.show()
+
